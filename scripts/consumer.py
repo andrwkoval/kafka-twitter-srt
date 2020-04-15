8d0e14838f32042f41ddb931d3def111797b1da0
Andrii <andykoval@ucu.edu.ua>
2020-04-15 03:02:54 +0300

    All questions ready

--- scripts/consumer.py
+++ scripts/consumer.py
@@ -90,7 +90,7 @@ def aggregated_statistics(consumer, report_time: datetime.datetime):
         if tweet.user_id not in users.keys():
             users[tweet.user_id] = [0, 0, 0]
         user_tweet_freq = users[tweet.user_id]
-        created_at = datetime.datetime.strptime(tweet["time"], "%d/%m/%Y %H:%M:%S")
+        created_at = datetime.datetime.strptime(tweet["created_at"], "%d/%m/%Y %H:%M:%S")
         diff = report_time - created_at
         if diff.seconds >= 3 * 60 * 60:
             continue
@@ -102,6 +102,37 @@ def aggregated_statistics(consumer, report_time: datetime.datetime):
             user_tweet_freq[2] += 1
         users[tweet.user_id] = user_tweet_freq
     return users
+
+
+def most_producing_accounts(consumer, report_time: datetime.datetime, n: int):
+    users = dict()
+    for tweet in all_tweets(consumer):
+        if tweet.user_id not in users.keys():
+            users[tweet.user_id] = 0
+        created_at = datetime.datetime.strptime(tweet["created_at"], "%d/%m/%Y %H:%M:%S")
+        diff = report_time - created_at
+        if diff.seconds < n * 60 * 60:
+            users[tweet.user_id] += 1
+
+    top_users = sorted(users.items(), key=lambda x: x[1], reverse=True)[:20]
+    return [i[0] for i in top_users]
+
+
+def most_popular_hashtags(consumer, report_time: datetime.datetime, n: int):
+    hashtags = dict()
+    for tweet in all_tweets(consumer):
+        created_at = datetime.datetime.strptime(tweet["created_at"], "%d/%m/%Y %H:%M:%S")
+        diff = report_time - created_at
+        if diff.seconds >= n * 60 * 60:
+            continue
+        local_tags = list(filter(lambda x: x.startswith("#"), tweet.content.split()))
+        for tag in local_tags:
+            if tag not in hashtags.keys():
+                hashtags[tag] = 0
+            hashtags[tag] += 1
+
+    top_hashtags = sorted(hashtags.items(), key=lambda x: x[1], reverse=True)[:10]
+    return [i[0] for i in top_hashtags]
 
 if __name__ == '__main__':
     fire.Fire(create_report)
